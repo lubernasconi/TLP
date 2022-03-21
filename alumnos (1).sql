4877b4f93f03da847e3f0eed52a7abe3b5d1b646
Lucía Bernasconi <86141079+lubernasconi@users.noreply.github.com>
2022-03-21 10:30:15 -0300

    Add files via upload

---
+++ alumnos (1).sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-03-2022 a las 14:23:47
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bernasconi_medran`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `Id_Legajo` int(11) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `Apellido` varchar(30) NOT NULL,
+  `Domicilio` varchar(50) NOT NULL,
+  `Teléfono` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
+INSERT INTO `alumnos` (`Id_Legajo`, `Nombre`, `Apellido`, `Domicilio`, `Teléfono`) VALUES
+(1, 'Lucía', 'Bernasconi', 'José Jening 769', 155334272),
+(2, 'Celeste', 'Medran', 'Molle 120', 153508149),
+(3, 'Camila', 'Linaza', 'Parque Velez Sarfiled ', 152883098),
+(4, 'Lara', 'Muzzio', 'Independencia 300', 1513549195),
+(5, 'Isabel', 'Ribulgo', 'Mariano Fragueiro 4450', 4704379),
+(6, 'Joaquina', 'Rivera', 'Mariano Moreno', 157629959),
+(7, 'Jazmin', 'Lastra', 'Bv Illia 500', 156826795),
+(8, 'Lucia', 'Villagra', 'Rio Negro 765', 156193200);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`Id_Legajo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `Id_Legajo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
