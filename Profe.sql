f8d397c68e7a42db4b55398904fef0dfb6e80433
Lucía Bernasconi <86141079+lubernasconi@users.noreply.github.com>
2022-04-04 10:08:41 -0300

    Add files via upload

---
+++ Profe.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-04-2022 a las 15:05:13
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `profesores`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Profe`
+--
+
+CREATE TABLE `profe` (
+  `Id_profesor` int(11) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `DNI` int(11) NOT NULL,
+  `Antiguedad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `Profe`
+--
+
+INSERT INTO `Profe` (`Id_profesor`, `Nombre`, `DNI`, `Antiguedad`) VALUES
+(1, 'Silvana Burgos', 25152339, 5),
+(2, 'MEDRAN NADIA CELESTE', 30331653, 10),
+(3, 'MEDRAN GUILLERMINA', 55005787, 6),
+(4, 'BERNASCONI JUAN MANUEL', 29466664, 7),
+(5, 'VIGILANTI ILDA ANITA', 17490515, 35);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Profe`
+--
+ALTER TABLE `Profe`
+  ADD PRIMARY KEY (`Id_profesor`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Profe`
+--
+ALTER TABLE `Profe`
+  MODIFY `Id_profesor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
